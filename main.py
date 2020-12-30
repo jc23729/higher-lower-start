2a3b706546877befbbe9cb82139ba5659f65ed18
Juan Cruz <jc23729@gmail.com>
2020-12-30 18:34:50 +0000

    Displayed art, and generated random account from game data

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-###Break down the problem###
+from art import logo, vs
+from game_data import data
+import random
 
 
 # Display the art
+print(logo)
 
+# Generate a random account from game data and split up into two accounts, A and B
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+  account_b = random.choice(data)
 
-# Generate a random account from game data
-
-
-
-# How do we format the data account into a printable format
+# How do we format the data account into a printable format//So we know that the data is a dictionary and we accesss with a key value pair
 
 
 # Ask the user for a guess
