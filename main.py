cc21e5627b5531f7162716b2db143ea37dfecdde
Juan Cruz <jc23729@gmail.com>
2020-12-30 18:12:52 +0000

    Break down the problem

---
+++ main.py
@@ -0,0 +1,23 @@
+###Break down the problem###
+
+# Ask the user for a guess
+
+
+# Check if user is correct
+
+
+# Get the follower count of each account
+
+
+# Use if statement to check if user is correct
+
+# Give user feedback on their guess
+
+# Score keeping
+
+# Make the game repeatable
+
+# Making account at position B become the next account at position a
+
+
+# Clear the screen
